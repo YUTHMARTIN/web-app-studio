7755921e7f1ea7b01a532ad62430c024b021791e
Yuth <2024073khim@aupp.edu.kh>
2026-01-06 22:37:07 +0700

    Fix bug LanguageProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { LanguageProvider } from "./contexts/LanguageContext";
+import { LanguageProvider } from "@/contexts/LanguageContext";
 import App from "./App.tsx";
 import "./index.css";
 
